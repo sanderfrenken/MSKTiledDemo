ce7774873e912dd7c3bb8019a9fbf7ce3aa9b380
Frenken, S.H. (Sander) <>
2024-09-04 16:11:23 +0200

    Initial commit

---
+++ MSKTiledDemo/GameViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let skView = self.view as? SKView else {
+            fatalError("SKView could not be referenced")
+        }
+
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.preferredFramesPerSecond = 60
+        skView.shouldCullNonVisibleNodes = true
+        skView.ignoresSiblingOrder = true
+
+        let scene: SKScene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
